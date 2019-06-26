a3297e5c84f621fdae768ab6c867a544b1a35dca
Sun.Songsong <sss09217313@163.com>
2019-06-26 23:21:46 +0800

    Init commit
    Change-Id: I60848a5dc6f13a640b239cfea3501b20d31e4aab

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**************************************************************************
+**
+** Copyright (C) 2019 sss0921.
+** Contact: sss09217313@163.com
+**
+** This file is part of Drawing.
+**
+**************************************************************************/
+
+#include <QApplication>
+
+int main(int agrc, char *agrv[])
+{
+    QApplication app(agrc, agrv);
+
+    return app.exec();
+}
